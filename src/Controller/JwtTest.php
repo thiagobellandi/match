d91aad8e815613b984743a8e874f266777206aa1
Thiago <t.bellandi@gmail.com>
2022-03-13 23:21:28 +0000

    atualizacao

--- src/Controller/JwtTest.php
+++ src/Controller/JwtTest.php
@@ -26,7 +26,9 @@ class JwtTest
         $params['dateend'] = "2022-11-10";
         $params['ltoken'] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
 */
-//        $params['login'] = "admin@match.mt";
+
+        $params['student'] = "75c9920a9e28b3013da9447c4daba24d";
+        $params['course'] = "8f543ad54def95e6d2a58625f86ff8fe";
         $params['ltoken'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9';
 
         $jwt = \Firebase\JWT\JWT::encode($params, $key, 'HS256');
